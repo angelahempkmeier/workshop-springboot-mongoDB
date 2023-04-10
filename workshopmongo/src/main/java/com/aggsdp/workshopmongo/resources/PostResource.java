94f0b24ec2dc5e5e07ccf96051533e753211267d
aggsdp <aggsdpz@gmail.com>
2023-04-09 22:20:36 -0300

    busca com varios criterios

--- workshopmongo/src/main/java/com/aggsdp/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/aggsdp/workshopmongo/resources/PostResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,4 +35,18 @@ public class PostResource {
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 }
